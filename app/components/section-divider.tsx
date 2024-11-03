4d666d48fb280512187d7df2a96e2ea81d43d2d7
hackerdud3 <nandikantivinay35@gmail.com>
2024-11-03 13:13:36 -0800

    Made section divider mobile friendly and responsive

--- app/components/section-divider.tsx
+++ app/components/section-divider.tsx
@@ -5,7 +5,7 @@ import { Card } from "@nextui-org/react";
 export function SectionDivider() {
   return (
     <div className="relative w-full py-1">
-      <Card className="h-10 bg-background/40 backdrop-blur-md border-t-1 border-b-1 border-white/10 overflow-hidden rounded-none" />
+      <Card className="lg:h-10 h-5 bg-background/40 backdrop-blur-md border-t-1 border-b-1 border-white/10 overflow-hidden rounded-none" />
     </div>
   );
 }
